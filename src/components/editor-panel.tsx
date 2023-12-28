5a198f17a13f4fa55dacac77f06c600711c82f4c
Francisco Moretti <franciscoemoretti@gmail.com>
2023-12-28 20:37:54 +0000

    scroll area editor

--- src/components/editor-panel.tsx
+++ src/components/editor-panel.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Editor as NovelEditor } from "novel";
 import { type JSONContent } from "@tiptap/core";
 import { type Editor } from "@tiptap/core";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
 
 export default function EditorPanel({
   onDebouncedUpdate,
@@ -31,29 +32,31 @@ export default function EditorPanel({
       <div className="absolute right-5 top-5 z-10 mb-5 rounded-lg bg-stone-100 px-2 py-1 text-sm text-stone-400">
         {saveStatus}
       </div>
-      <NovelEditor
-        onCreate={(editor) => {
-          setSaveStatus("Unsaved");
-          if (editor && setEditor) {
-            setEditor(editor);
-          }
-        }}
-        onDebouncedUpdate={async (editor) => {
-          setSaveStatus("Saving...");
-          if (onDebouncedUpdate) {
-            await onDebouncedUpdate(editor);
-          }
-          // Simulate a delay in saving.
-          setTimeout(() => {
-            setSaveStatus("Saved");
-          }, 500);
-        }}
-        completionApi={completionApi}
-        completionId={completionId}
-        storageKey={storageKey}
-        defaultValue={defaultValue}
-        disableLocalStorage={disableLocalStorage}
-      />
+      <ScrollArea className="h-[1000px] overflow-y-auto rounded-md border p-4">
+        <NovelEditor
+          onCreate={(editor) => {
+            setSaveStatus("Unsaved");
+            if (editor && setEditor) {
+              setEditor(editor);
+            }
+          }}
+          onDebouncedUpdate={async (editor) => {
+            setSaveStatus("Saving...");
+            if (onDebouncedUpdate) {
+              await onDebouncedUpdate(editor);
+            }
+            // Simulate a delay in saving.
+            setTimeout(() => {
+              setSaveStatus("Saved");
+            }, 500);
+          }}
+          completionApi={completionApi}
+          completionId={completionId}
+          storageKey={storageKey}
+          defaultValue={defaultValue}
+          disableLocalStorage={disableLocalStorage}
+        />
+      </ScrollArea>
     </div>
   );
 }
