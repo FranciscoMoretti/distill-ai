6744be1b86ccff81d9b26c4b6d69a03974dd0aad
Francisco Moretti <franciscoemoretti@gmail.com>
2023-12-28 20:08:04 +0000

    github repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className=" container flex min-h-screen flex-col items-center gap-12 px-4 py-16 ">
         <a
-          href="https://github.com/steven-tey/novel"
+          href="https://github.com/FranciscoMoretti/destill-ai"
           target="_blank"
           className="absolute bottom-5 left-5 z-10 max-h-fit rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto sm:top-5"
         >
