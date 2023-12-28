252922824c16ce729ce41104ae449ddbb6895151
Francisco Moretti <franciscoemoretti@gmail.com>
2023-12-28 11:57:40 +0000

    simple connection between 3 editors

--- src/components/multi-editor.tsx
+++ src/components/multi-editor.tsx
@@ -1,19 +1,21 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import EditorPanel from "@/components/editor";
 import { extractBoldText } from "@/lib/text-extractor";
 import { type Editor, extensions, generateJSON } from "@tiptap/core";
+import { useCompletion } from "ai/react";
+import { toast } from "sonner";
 
 export default function MultiEditor() {
   const [mainContent, setMainContent] = useState<string>("");
+  const [outlineContent, setOutlineContent] = useState<string>("");
   const [outlineEditor, setOutlineEditor] = useState<Editor | null>(null);
   const [summaryEditor, setSummaryEditor] = useState<Editor | null>(null);
 
   useEffect(() => {
     if (mainContent && outlineEditor) {
       const filteredContent = extractBoldText(mainContent);
-      console.log(filteredContent);
 
       outlineEditor.commands.setContent(
         generateJSON(
@@ -23,6 +25,38 @@ export default function MultiEditor() {
       );
     }
   }, [mainContent]);
+
+  useEffect(() => {
+    if (outlineContent) {
+      console.log({ outlineContent });
+      if (outlineContent) {
+        complete(outlineContent);
+      }
+    }
+  }, [outlineContent]);
+
+  const { complete, completion, isLoading, stop } = useCompletion({
+    id: "ai_summary",
+    api: "/api/generate",
+    onFinish: (_prompt, completion) => {
+      summaryEditor?.commands.setTextSelection({
+        from: summaryEditor.state.selection.from - completion.length,
+        to: summaryEditor.state.selection.from,
+      });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  const prev = useRef("");
+
+  // Insert chunks of the generated text
+  useEffect(() => {
+    const diff = completion.slice(prev.current.length);
+    prev.current = completion;
+    summaryEditor?.commands.insertContent(diff);
+  }, [isLoading, summaryEditor, completion]);
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
@@ -41,7 +75,15 @@ export default function MultiEditor() {
         setEditor={setOutlineEditor}
         completionApi={"/api/complete"}
         // TODO: Fix this hack that produces a new editor everytime when I get access to the editor command API
-        onDebouncedUpdate={console.log}
+        onDebouncedUpdate={(editor) => {
+          if (editor) {
+            // TODO not update on every update
+            const textString = editor?.getText();
+            if (textString != outlineContent) {
+              setOutlineContent(textString);
+            }
+          }
+        }}
         defaultValue={DEFAULT_OUTLINE_TEXT}
         disableLocalStorage={false}
         storageKey="novel__outline"
