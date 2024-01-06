505945616c0c0a4ac457d4e610366551b41f6944
Francisco Moretti <franciscoemoretti@gmail.com>
2024-01-06 20:45:02 +0000

    small siteheader sign-in-out

--- src/components/site/site-header.tsx
+++ src/components/site/site-header.tsx
@@ -6,8 +6,41 @@ import { buttonVariants } from "@/components/plate-ui/button";
 import { MainNav } from "@/components/site/main-nav";
 import { ThemeToggle } from "@/components/site/theme-toggle";
 import { cn } from "@/lib/utils";
+import { getServerAuthSession } from "@/server/auth";
 
-export function SiteHeader({ className = "" }: { className?: string }) {
+function NavCommonButtons() {
+  return (
+    <nav className="flex items-center space-x-1">
+      <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
+        <div
+          className={buttonVariants({
+            size: "sm",
+            variant: "ghost",
+          })}
+        >
+          <Icons.gitHub className="h-4 w-4" />
+          <span className="sr-only">GitHub</span>
+        </div>
+      </Link>
+      <Link href={siteConfig.links.twitter} target="_blank" rel="noreferrer">
+        <div
+          className={buttonVariants({
+            size: "sm",
+            variant: "ghost",
+          })}
+        >
+          <Icons.twitter className="h-5 w-5 fill-current" />
+          <span className="sr-only">Twitter</span>
+        </div>
+      </Link>
+      <ThemeToggle />
+    </nav>
+  );
+}
+
+export async function SiteHeader({ className = "" }: { className?: string }) {
+  const session = await getServerAuthSession();
+
   return (
     <header
       className={cn(
@@ -18,38 +51,17 @@ export function SiteHeader({ className = "" }: { className?: string }) {
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
+          <NavCommonButtons />
+          <nav>
             <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
+              href={session ? "/api/auth/signout" : "/api/auth/signin"}
+              className={cn(
+                buttonVariants({ variant: "secondary", size: "sm" }),
+                "px-4",
+              )}
             >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.gitHub className="h-4 w-4" />
-                <span className="sr-only">GitHub</span>
-              </div>
+              {session ? "Sign out" : "Sign in"}
             </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
-            <ThemeToggle />
           </nav>
         </div>
       </div>
