0343ee5eb053a1894542d6fde4678dba745f5211
Francisco Moretti <franciscoemoretti@gmail.com>
2024-01-06 10:01:58 +0000

    fix delection of tab

--- src/components/multi-editor-ui.tsx
+++ src/components/multi-editor-ui.tsx
@@ -45,6 +45,15 @@ export function MultiEditorUi({ className = "" }: { className?: string }) {
     }
   }, [width]);
 
+  function handleTabValueChange(): ((value: string) => void) | undefined {
+    return (value) => {
+      if (value) {
+        // Only change if another is being selected (don't deselect current)
+        setTabValue(value as EditorType);
+      }
+    };
+  }
+
   const TAB_CN =
     "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-background data-[state=on]:text-foreground data-[state=on]:shadow-sm";
 
@@ -63,7 +72,7 @@ export function MultiEditorUi({ className = "" }: { className?: string }) {
           )}
           type="single"
           value={tabValue}
-          onValueChange={(value) => setTabValue(value as EditorType)}
+          onValueChange={handleTabValueChange()}
         >
           <ToggleGroupItem
             size={"sm"}
@@ -99,7 +108,7 @@ export function MultiEditorUi({ className = "" }: { className?: string }) {
           )}
           type="single"
           value={tabValue}
-          onValueChange={(value) => setTabValue(value as EditorType)}
+          onValueChange={handleTabValueChange()}
         >
           <ToggleGroupItem
             size={"sm"}
