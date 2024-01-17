a5f94245c545ca580c9306828a55cc9bc91f2c48
Francisco Moretti <franciscoemoretti@gmail.com>
2024-01-17 19:01:35 +0000

    fix auth on home route

--- src/middleware.ts
+++ src/middleware.ts
@@ -42,11 +42,5 @@ export default withAuth(
 );
 
 export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/:path*",
-    "/editor/:path*",
-    "/login",
-    "/register",
-  ],
+  matcher: ["/dashboard/:path*", "/editor/:path*", "/login", "/register"],
 };
