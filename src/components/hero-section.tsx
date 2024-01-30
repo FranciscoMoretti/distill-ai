52f85e870fa894e9445125005867090a49ea7ba9
Francisco Moretti <franciscoemoretti@gmail.com>
2024-01-30 10:55:29 +0000

    better hero section

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -20,13 +20,13 @@ export function HeroContent({
   return (
     <WavyBackground
       backgroundFill={backgroundColor}
-      className={"mx-auto max-w-4xl pb-40"}
+      className={"mx-auto max-w-4xl text-balance px-10 pb-40"}
       containerClassName={className}
     >
-      <p className="inter-var text-center text-2xl font-bold  md:text-4xl lg:text-7xl">
+      <p className="inter-var text-center text-3xl font-bold sm:text-4xl md:text-6xl lg:text-9xl">
         Effortless Summaries
       </p>
-      <p className="inter-var mt-4 text-center text-base font-normal md:text-lg">
+      <p className="inter-var mt-4 text-center text-xl font-normal md:text-3xl">
         Curate your notes, let AI craft the summary
       </p>
     </WavyBackground>
