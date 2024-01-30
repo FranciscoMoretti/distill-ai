7b324dc6e582ff4c1e4a63d08312ab2c1dd12d45
Francisco Moretti <franciscoemoretti@gmail.com>
2024-01-30 10:32:29 +0000

    themable hero

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -3,11 +3,30 @@ import { WavyBackground } from "@/components/wavy-background";
 
 export function HeroSection() {
   return (
-    <WavyBackground className="mx-auto max-w-4xl pb-40">
-      <p className="inter-var text-center text-2xl font-bold text-white md:text-4xl lg:text-7xl">
+    <>
+      <HeroContent className="hidden dark:flex" backgroundColor="black" />;
+      <HeroContent className="flex dark:hidden" backgroundColor="white" />
+    </>
+  );
+}
+
+export function HeroContent({
+  backgroundColor,
+  className = "",
+}: {
+  backgroundColor: string;
+  className: string;
+}) {
+  return (
+    <WavyBackground
+      backgroundFill={backgroundColor}
+      className={"mx-auto max-w-4xl pb-40"}
+      containerClassName={className}
+    >
+      <p className="inter-var text-center text-2xl font-bold  md:text-4xl lg:text-7xl">
         Effortless Summaries
       </p>
-      <p className="inter-var mt-4 text-center text-base font-normal text-white md:text-lg">
+      <p className="inter-var mt-4 text-center text-base font-normal md:text-lg">
         Curate your preferences, let AI craft the summary
       </p>
     </WavyBackground>
