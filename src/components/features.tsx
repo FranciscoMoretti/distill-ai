e84f6d73c5dabbbec0a74a7cd32f3fa335b41f0e
Francisco Moretti <franciscoemoretti@gmail.com>
2024-02-05 19:49:01 +0000

    improve cards

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -19,30 +19,34 @@ export default function Features() {
         </h2>
       </div>
       <div className="container px-4 md:px-6">
-        <div className="grid gap-6 lg:grid-cols-3 lg:gap-12">
-          <Card className="">
-            <CardContent className="flex flex-col items-center space-y-4 pt-6">
+        <div className="grid gap-6 lg:grid-cols-3 lg:gap-8">
+          <Card className="rounded-xl">
+            <CardContent className="flex flex-col items-center space-y-4  pt-6">
               <Icons.folderTree className="h-12 w-12 " />
-              <CardTitle>Effortless Organization</CardTitle>
+              <CardTitle className="text-center">
+                Effortless Organization
+              </CardTitle>
               <CardDescription className="text-center">
                 Input and edit raw notes seamlessly.
               </CardDescription>
             </CardContent>
           </Card>
-          <Card className="">
+          <Card className="rounded-xl">
             <CardContent className="flex flex-col items-center space-y-4 pt-6">
               <Icons.magic className="h-12 w-12 " />
-              <CardTitle>AI-Powered Summaries</CardTitle>
+              <CardTitle className="text-center">
+                AI-Powered Summaries
+              </CardTitle>
               <CardDescription className="text-center">
                 Quick and efficient text processing for instant personalized
                 summaries.
               </CardDescription>
             </CardContent>
           </Card>
-          <Card className="">
+          <Card className="rounded-xl">
             <CardContent className="flex flex-col items-center space-y-4 pt-6">
               <Icons.settings className="h-12 w-12" />
-              <CardTitle>Customizable Output</CardTitle>
+              <CardTitle className="text-center">Customizable Output</CardTitle>
               <CardDescription className="text-center">
                 Options to tailor the summary output to your specific needs.
               </CardDescription>
